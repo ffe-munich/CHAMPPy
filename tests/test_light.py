313cb59efba8a5174056b7c470318cc1be764461
Florian Biedenbach <fbiedenbach@ffe.de>
2026-02-16 17:01:34 +0100

    tests

---
+++ tests/test_light.py
@@ -0,0 +1,43 @@
+"""
+Light tests for loading existing parameters and mobility profile generation using MobModel.
+Ensures that parameters are loaded correctly and mobility profiles are generated as expected.
+"""
+import os
+from champpy.core.mobility.parameterization import ParamsLoader
+from champpy.core.mobility.mobility_model import MobModel, UserParamsMobModel
+import pandas as pd
+import pytest
+
+@pytest.fixture
+def params_folder():
+    return os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "params")
+
+@pytest.fixture
+def params_loader(params_folder):
+    return ParamsLoader(params_folder)
+
+def test_params_info(params_loader):
+    info_df = params_loader.load_info()
+    # Check that info_df is a DataFrame and not empty
+    assert isinstance(info_df, pd.DataFrame)
+    assert not info_df.empty
+
+def test_mobility_model_generation(params_loader):
+    my_params1 = params_loader.load_params(id_params=1)
+    assert my_params1 is not None
+    mob_model = MobModel(model_params=my_params1)
+    user_params = UserParamsMobModel(
+        number_vehicles=20,
+        start_date=pd.Timestamp("2025-01-01-01:00:00"),
+        end_date=pd.Timestamp("2025-12-31-23:00:00"),
+        random_seed=2
+    )
+    mob_profiles = mob_model.generate_mob_profiles(user_params=user_params)
+    # Check that mob_profiles has a DataFrame attribute and is not empty
+    assert hasattr(mob_profiles, 'df')
+    assert isinstance(mob_profiles.df, pd.DataFrame)
+    assert not mob_profiles.df.empty
+
+
+
+
---
+++ tests/test_validation.py
@@ -0,0 +1,45 @@
+"""Test of the validation module of champpy."""
+
+import pytest
+import pandas as pd
+from champpy.core.mobility.mobility_validation import MobPlotter, UserParamsMobPlotter, MobilityCharacteristics
+from champpy.utils.time_utils import TypeDays
+
+def test_mobility_characteristics_default(mob_data1):
+    mob_data = mob_data1
+    mob_char = MobilityCharacteristics(mob_data)
+    # Check that char_df is a DataFrame and has expected columns
+    assert isinstance(mob_char.df, pd.DataFrame)
+    assert mob_char.df.shape[0] == 1 # Default only one row
+    expected_columns = ['typeday', 'daily_kilometrage', 'daily_journey_time', 
+                        'number_journeys_per_day', 'share_days_with_journeys', 'locations', 
+                        'share_of_time_at_locations']
+    assert all(col in mob_char.df.columns for col in expected_columns)
+
+def test_mobility_characteristics_grouping_vehicle(mob_data1):
+    mob_data = mob_data1
+    typedays = TypeDays(groups=[[0,1,2,3,4],[5,6]]) # weekday vs weekend
+    mob_char = MobilityCharacteristics(mob_data, grouping='vehicle', typedays=typedays)
+    # Check that char_df is a DataFrame and has expected number of rows
+    assert mob_char.df.shape[0] == 2 # Two typdays: weekday and weekend
+    assert  isinstance(mob_char.df.daily_kilometrage[0],list) # must contain a list due to vehicle grouping
+
+def test_mobility_characteristics_grouping_day(mob_data1):
+    mob_data = mob_data1
+    typedays = TypeDays(groups=[[0],[1],[2],[3],[4],[5],[6]]) # every day separate except weekend combined
+    mob_char = MobilityCharacteristics(mob_data, grouping='day', typedays=typedays)
+    # Check that char_df is a DataFrame and has expected number of rows
+    assert mob_char.df.shape[0] == 7 
+    assert  isinstance(mob_char.df.daily_kilometrage[0],list) # must contain a list due to day grouping
+
+def test_mob_plotter_onecluster(mob_data1):
+    mob_data = mob_data1
+    user_params = UserParamsMobPlotter(show=True, save_plot = True, filename="test.dt")  # just to test initialization
+    mob_plotter = MobPlotter(user_params=user_params)
+    mob_plotter.plot_mob_data(mob_data)
+
+def test_mob_plotter_nclusters(mob_data3):
+    mob_data = mob_data3
+    user_params = UserParamsMobPlotter(show=True, save_plot = True, filename="test.dt")  # just to test initialization
+    mob_plotter = MobPlotter(user_params=user_params)
+    mob_plotter.plot_mob_data(mob_data)
