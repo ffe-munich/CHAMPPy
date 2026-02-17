9382e9079e4305acf51009a3355d6397c5dc8077
Florian Biedenbach <fbiedenbach@ffe.de>
2026-02-17 09:16:16 +0100

    formatting __init__

--- src/champpy/__init__.py
+++ src/champpy/__init__.py
@@ -1,18 +1,18 @@
 """CHAMPPy package."""
-
-from champpy.core.mobility.mobility_components import Logbooks, Vehicles, Clusters, Locations
-from champpy.core.mobility.mobility_data import MobData
-from champpy.core.mobility.mobility_cleaning import MobDataCleaner, UserParamsCleaning, LimitConfig
-from champpy.core.mobility.parameterization import Parameterizer, UserParamsParameterizer, ParamsLoader
-from champpy.core.mobility.mobility_model import MobModel, UserParamsMobModel
-from champpy.core.mobility.mobility_validation import MobPlotter, UserParamsMobPlotter, MobilityCharacteristics
-from champpy.core.charging.charging_model import ChargingModel, ChargingArray, UserParamsChargingModel, ChargingData
-from champpy.core.charging.charging_validation import (
+# TODO: Challangen. Ist das der rcihtige platz, um die Module zu importieren?
+from champpy.core.mobility.mobility_components import Logbooks, Vehicles, Clusters, Locations  # noqa: F401
+from champpy.core.mobility.mobility_data import MobData  # noqa: F401
+from champpy.core.mobility.mobility_cleaning import MobDataCleaner, UserParamsCleaning, LimitConfig  # noqa: F401
+from champpy.core.mobility.parameterization import Parameterizer, UserParamsParameterizer, ParamsLoader  # noqa: F401
+from champpy.core.mobility.mobility_model import MobModel, UserParamsMobModel  # noqa: F401
+from champpy.core.mobility.mobility_validation import MobPlotter, UserParamsMobPlotter, MobilityCharacteristics  # noqa: F401
+from champpy.core.charging.charging_model import ChargingModel, ChargingArray, UserParamsChargingModel, ChargingData  # noqa: F401
+from champpy.core.charging.charging_validation import (  # noqa: F401
     ChargingPlotter,
     UserParamsChargingPlotter,
     ChargingCharacteristics,
 )
-from champpy.utils.time_utils import TypeDays
+from champpy.utils.time_utils import TypeDays  # noqa: F401
 from champpy.utils.logging import setup_logging
 
-setup_logging()
+setup_logging() # TODO: Wo packe ich das am besten hin?
