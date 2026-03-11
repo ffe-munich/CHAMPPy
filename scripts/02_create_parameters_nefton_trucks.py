2d37205535a07a8434cd81da9950dbaeefde1577
Florian Biedenbach <fbiedenbach@ffe.de>
2026-03-11 12:52:04 +0100

    add new parameters
    Added the parameters for the mobility profiles used i nthe paper:
    - Vans based on rem2030
    - trucks based on nefton
    - cars based on kid 2010

--- scripts/02_create_parameters_nefton_trucks.py
+++ scripts/02_create_parameters_nefton_trucks.py
@@ -6,16 +6,21 @@ import champpy
 # --------------------------------------------------------
 # Load example raw logbook data from CSV
 base_dir = os.getcwd()
-logbook_path = os.path.join(base_dir, "data", "raw_data", "example1", "t_logbook.csv")
+folder_name = "nefton_trucks"
+logbook_path = os.path.join(base_dir, "data", folder_name, "t_logbook.csv")
 print(logbook_path)
+if not os.path.exists(logbook_path):
+    raise FileNotFoundError(f"Logbook file not found: {logbook_path}")
 raw_logbook_df = pd.read_csv(logbook_path, parse_dates=["dep_dt", "arr_dt"])
 
 # Load example raw vehicle data from CSV
-vehicle_path = os.path.join(base_dir, "data", "raw_data", "example1", "t_vehicle.csv")
+vehicle_path = os.path.join(base_dir, "data", folder_name, "t_vehicle.csv")
+if not os.path.exists(vehicle_path):
+    raise FileNotFoundError(f"Vehicle file not found: {vehicle_path}")
 raw_vehicle_df = pd.read_csv(vehicle_path, parse_dates=["first_day", "last_day"], date_format="%d-%b-%Y")
 
 # Create raw mobility profiles
-raw_profiles = champpy.MobData(input_logbooks_df=raw_logbook_df, input_vehicles_df=raw_vehicle_df)
+raw_profiles = champpy.MobProfiles(input_logbooks_df=raw_logbook_df, input_vehicles_df=raw_vehicle_df)
 
 # Clean the raw mobility profiles
 # --------------------------------------------------------
@@ -23,40 +28,40 @@ raw_profiles = champpy.MobData(input_logbooks_df=raw_logbook_df, input_vehicles_
 user_params_cleaning = champpy.UserParamsCleaning(
     speed=champpy.LimitConfig(min_value=0.01, min_method="delete", max_value=120.0, max_method="cap"),
     duration=champpy.LimitConfig(min_value=0.25, min_method="delete", max_value=8.0, max_method="cap"),
-    distance=champpy.LimitConfig(min_value=0.5, min_method="delete", max_value=500.0, max_method="cap"),
+    distance=champpy.LimitConfig(min_value=0.5, min_method="delete", max_value=700.0, max_method="cap"),
     temp_res=0.25,  # Temporal resolution in hours
     print_summary=True,
 )
-data_cleaner = champpy.MobDataCleaner(user_params=user_params_cleaning)
+data_cleaner = champpy.MobProfilesCleaner(user_params=user_params_cleaning)
 
 # Apply data cleaner on your raw mobility profiles
 mob_profile_cleaned = data_cleaner.clean(raw_profiles)
 
 # Update location labels
 locations_df = mob_profile_cleaned.locations.df
-locations_df["label"] = ["Driving", "Home", "Other location"]
+locations_df["label"] = ["Driving", "Depot", "Other location", "Rest area", "Industrial area"]
 mob_profile_cleaned.locations.update_locations(locations_df)
 
 # Parameterization
 # --------------------------------------------------------
 # Define user params for the parameterization
-user_params = champpy.UserParamsParameterizer(
-    description="Example parameterization 1",  # Define a description for the parameter set
-    vehicle_type="Van",  # Type of vehicle the parameters apply to (e.g., "Car", "Van", "Truck")
+user_params_param = champpy.UserParamsParameterizer(
+    description="Trucks based on Nefton Dataset (https://doi.org/10.5281/zenodo.7599687)",  # Define a description for the parameter set
+    vehicle_type="Truck",  # Type of vehicle the parameters apply to (e.g., "Car", "Van", "Truck")
     temp_res=0.25,  # Temporal resolution in hours
 )
 # Create an instance of the Parameterizer
-example_parameterizer = champpy.Parameterizer(user_params)
+my_parameterizer = champpy.Parameterizer(user_params_param)
 
 # calculate the model parameters based on the cleaned mobility profiles
-model_params = example_parameterizer.calc_params(mob_profile_cleaned)
+model_params = my_parameterizer.calc_params(mob_profile_cleaned)
 
 # Generate synthetic mobility profiles
 # --------------------------------------------------------
 mob_model = champpy.MobModel(model_params=model_params)
 user_params_mob = champpy.UserParamsMobModel(
     number_vehicles=50,
-    start_date=pd.Timestamp("2025-01-01-01:00:00"),
+    start_date=pd.Timestamp("2025-01-01-00:00:00"),
     end_date=pd.Timestamp("2025-12-31-23:00:00"),
 )
 mob_profiles = mob_model.generate_mob_profiles(user_params=user_params_mob)
@@ -65,18 +70,16 @@ mob_profiles = mob_model.generate_mob_profiles(user_params=user_params_mob)
 # --------------------------------------------------------
 # Create a copy of the cleaned mobility profiles and add the modeld profiles for comparison using add_mob_data()
 mob_data_merged = mob_profile_cleaned.copy()
-mob_data_merged.add_mob_data(mob_profiles, old_cluster_label="Ref", new_cluster_label="Model")
+mob_data_merged.add_mob_profiles(mob_profiles, old_cluster_label="Ref", new_cluster_label="Model")
 
 # Initialize user parameters for plotting the mobiltiy profiles
-user_params_plot = champpy.UserParamsMobPlotter(
-    filename="example_mobility_profiles_validation_plot.html", clustering=False
-)
+user_params_plot = champpy.UserParamsMobPlotter(filename="validation_nefton_trucks.html", clustering=True)
 
 # Create instance of the mobility plotter
 mobplot = champpy.MobPlotter(user_params_plot)
 
 # Plot the mobility profiles for the merged data (ref + model)
-mobplot.plot_mob_data(mob_data_merged)
+mobplot.plot_mob_profiles(mob_data_merged)
 
 # Save Parameters
 params_loader = champpy.ParamsLoader()
