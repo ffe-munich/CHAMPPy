1cce2fe8cb4bb7ec3c27994b63d5b7e1e1158ded
Florian Biedenbach <f.biedenbach@campus.tu-berlin.de>
2026-03-11 22:34:55 +0100

    update title of documentation

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,7 @@ language = "[en]"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
+html_title = "CHAMPPy Documentation"
 html_static_path = ["_static"]
 html_theme_options = {
     "navigation_with_keys": True,
